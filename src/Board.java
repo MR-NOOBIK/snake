9808346c8865c8b150892edf191ea9f490a84e0b
Mr.Noobik <mr.noobik.git@gmail.com>
2017-10-19 13:48:53 +0300

    start version snake java

---
+++ src/Board.java
@@ -0,0 +1,290 @@
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Board extends JLabel implements ActionListener {
+
+    private final int B_WIDTH = 1600;
+    private final int B_HEIGHT = 820;
+    private final int DOT_SIZE = 10;
+    private final int ALL_DOTS = 50;
+    private final int RAND_POS = 10;
+    private final int DELAY = 100;
+    private final int DELAY_BOOST = 50;
+    
+    
+    private final int x[] = new int[ALL_DOTS];
+    private final int y[] = new int[ALL_DOTS];
+    
+    private final int players[] = new int[20];
+
+    private int snake_size = 4;
+    private int apple_x;
+    private int apple_y;
+    private int score_for_apple = 10;
+
+    private boolean leftDirection = false;
+    private boolean rightDirection = false;
+    private boolean upDirection = false;
+    private boolean downDirection = true;
+    private boolean inGame = true;
+
+    private Timer timer;
+    private Image ball;
+    private Image apple;
+    private Image head;
+
+    public Board() {
+
+        addKeyListener(new TAdapter());
+//        setBackground(Color.black);
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File("images.jpeg"));
+        } catch (IOException ex) {
+            Logger.getLogger(Board.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Image dimg = img.getScaledInstance(B_WIDTH, B_HEIGHT, Image.SCALE_SMOOTH);
+        setIcon(new ImageIcon(dimg));
+        setFocusable(true);
+
+        setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
+        loadImages();
+        initGame();
+    }
+
+    private void loadImages() {
+
+        ImageIcon iid = new ImageIcon("dot.png");
+        ball = iid.getImage();
+
+        ImageIcon iia = new ImageIcon("apple.png");
+        apple = iia.getImage();
+
+        ImageIcon iih = new ImageIcon("head.png");
+        head = iih.getImage();
+    }
+
+    private void initGame() {
+
+        // draw snake
+        for (int z = 0; z < snake_size; z++) {
+            y[z] = 50 - z * DOT_SIZE;
+            x[z] = 50;
+        }
+
+        locateApple();
+
+        // start timer
+        timer = new Timer(DELAY, this);
+        timer.start();
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        doDrawing(g);
+    }
+
+    private void doDrawing(Graphics g) {
+
+        if (inGame) {
+
+            g.drawImage(apple, apple_x, apple_y, this);
+
+            for (int z = 0; z < snake_size; z++) {
+                if (z == 0) {
+                    g.drawImage(head, x[z], y[z], this);
+                } else {
+                    g.drawImage(ball, x[z], y[z], this);
+                }
+            }
+
+            Toolkit.getDefaultToolkit().sync();
+
+        } else {
+
+            gameOver(g);
+        }
+    }
+
+    private void gameOver(Graphics g) {
+
+        String msg = "Конец";
+        Font small = new Font("Helvetica", Font.BOLD, 14);
+        FontMetrics metr = getFontMetrics(small);
+
+        g.setColor(Color.green);
+        g.setFont(small);
+        g.drawString(msg, (B_WIDTH - metr.stringWidth(msg)) / 2, B_HEIGHT / 2);
+    }
+
+    private void checkApple() {
+
+        if ((x[1] == apple_x) && (y[1] == apple_y)) {
+
+            snake_size += score_for_apple;
+            locateApple();
+        }
+    }
+
+    private void move() {
+
+        // смещение координат тела змейки
+        for (int z = snake_size; z > 0; z--) {
+            x[z] = x[(z - 1)];
+            y[z] = y[(z - 1)];
+        }
+
+        // смещение координат головы змейки
+        if (leftDirection) {
+            x[0] -= DOT_SIZE;
+        }
+        if (rightDirection) {
+            x[0] += DOT_SIZE;
+        }
+        if (upDirection) {
+            y[0] -= DOT_SIZE;
+        }
+        if (downDirection) {
+            y[0] += DOT_SIZE;
+        }
+    }
+
+    private void checkCollision() {
+
+        // check self collision
+//        for (int z = snake_size; z > 0; z--) {
+//
+//            if ((z > snake_size) && (x[0] == x[z]) && (y[0] == y[z])) {
+//                inGame = false;
+//            }
+//        }
+
+        // check border collision
+        if (y[0] >= B_HEIGHT) {
+            inGame = false;
+        }
+
+        if (y[0] < 0) {
+            inGame = false;
+        }
+
+        if (x[0] >= B_WIDTH) {
+            inGame = false;
+        }
+
+        if (x[0] < 0) {
+            inGame = false;
+        }
+
+        if (!inGame) {
+            timer.stop();
+        }
+    }
+
+    private void locateApple() {
+
+        int r = (int) (Math.random() * RAND_POS);
+        apple_x = ((r * DOT_SIZE));
+
+        r = (int) (Math.random() * RAND_POS);
+        apple_y = ((r * DOT_SIZE));
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if (inGame) {
+
+            checkApple();
+            checkCollision();
+            move();
+        }
+
+        repaint();
+    }
+     
+    
+    
+    private class TAdapter extends KeyAdapter {
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+            int key = e.getKeyCode();
+
+            if ((key == KeyEvent.VK_LEFT) && (!rightDirection)) {
+                leftDirection = true;
+                upDirection = false;
+                downDirection = false;
+            }
+
+            if ((key == KeyEvent.VK_RIGHT) && (!leftDirection)) {
+                rightDirection = true;
+                upDirection = false;
+                downDirection = false;
+            }
+
+            if ((key == KeyEvent.VK_UP) && (!downDirection)) {
+                upDirection = true;
+                rightDirection = false;
+                leftDirection = false;
+            }
+
+            if ((key == KeyEvent.VK_DOWN) && (!upDirection)) {
+                downDirection = true;
+                rightDirection = false;
+                leftDirection = false;
+            }
+            
+            if (key == KeyEvent.VK_SPACE) {
+                System.out.println("press: " );
+                timer.setDelay(DELAY_BOOST);
+                
+            }
+        }
+        
+        @Override
+        public void keyReleased(KeyEvent e) {
+            int key = e.getKeyCode();
+            if (key == KeyEvent.VK_SPACE) {
+                System.out.println("released " );
+                timer.setDelay(DELAY);
+            }
+
+        }
+    }
+
+}
---
+++ src/Snake.java
@@ -0,0 +1,39 @@
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Snake extends JFrame {
+
+    public Snake() {
+
+        add(new Board());
+
+        setResizable(false);
+        pack();
+
+        setTitle("Snake 2");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame ex = new Snake();
+                ex.setVisible(true);
+            }
+        });
+    }
+}
